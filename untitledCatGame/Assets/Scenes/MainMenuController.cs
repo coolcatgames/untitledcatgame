c78a42afc1de8579f5395131e23260a78ef4a68a
vndresfc <andres13361@gmail.com>
2023-12-01 00:39:04 -0600

    Starter main menu
    First version of the menu. It is very basic and not done. Need to add background color and images.

---
+++ untitledCatGame/Assets/Scenes/MainMenuController.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuController : MonoBehaviour
+{
+    public CanvasGroup OptionPanel;
+
+    public void PlayGame()
+    {
+        // Loads the next scene
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
