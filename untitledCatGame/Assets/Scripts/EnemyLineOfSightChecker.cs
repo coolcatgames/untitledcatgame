6adcf44d1d350ad232a160dc1fbfb79384f24d08
Luke Wells <luke@lukewells.dev>
2023-12-04 18:00:04 -0600

    Beginning to add AI

---
+++ untitledCatGame/Assets/Scripts/EnemyLineOfSightChecker.cs
@@ -0,0 +1,60 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(SphereCollider))]
+public class EnemyLineOfSightChecker : MonoBehaviour
+{
+    public SphereCollider Collider;
+    public float FieldOfView = 90f;
+    public LayerMask LineOfSightLayers;
+
+    public delegate void GainSightEvent(Transform Target);
+    public GainSightEvent OnGainSight;
+    public delegate void LoseSightEvent(Transform Target);
+    public GainSightEvent OnLoseSight;
+
+    private Coroutine CheckForLineOfSightCoroutine;
+
+    private void awake() {
+        Collider = GetComponent<SphereCollider>();
+         
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (!CheckLineOfSight(other.transform))
+        {
+            CheckForLineOfSightCoroutine = StartCoroutine(CheckForLineOfSight(other.transform));
+        }
+    }
+
+    private void OnTriggerExit(Collider other){
+        OnLoseSight?.Invoke(other.transform);
+        if (CheckForLineOfSightCoroutine != null) {
+            StopCoroutine(CheckForLineOfSightCoroutine);
+        }
+    }
+
+    private bool CheckLineOfSight(Transform Target) {
+        Vector3 direction = (Target.transform.position - transform.position).normalized;
+        float dotProduct = Vector3.Dot(transform.forward, direction); // Gives a number between -1 and 1 that gives the forward direction
+        if (dotProduct >= Mathf.Cos(FieldOfView)){
+            if (Physics.Raycast(transform.position, direction, out RaycastHit hit, Collider.radius, LineOfSightLayers)){
+                OnGainSight?.Invoke(Target);
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    private IEnumerator CheckForLineOfSight(Transform Target){
+        WaitForSeconds Wait = new WaitForSeconds(0.5f);
+
+        while(!CheckLineOfSight(Target)){
+            yield return Wait;
+        }
+    }
+
+}
---
+++ untitledCatGame/Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,82 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+
+[RequireComponent(typeof(NavMeshAgent))]
+public class EnemyMovement : MonoBehaviour
+{
+    public LayerMask HidableLayers;
+    public EnemyLineOfSightChecker LineOfSightChecker;
+    public NavMeshAgent Agent;
+    [Range(-1, 1)]
+    [Tooltip("The lower the number, the better the hiding spot")]
+    public float HideSensitivity = 0;
+
+    private Coroutine MovementCoroutine;
+    private Collider[] Colliders = new Collider[10]; // Larger collider arrays are less performant but give more options for hiding
+
+    private void Awake(){
+        Agent = GetComponent<NavMeshAgent>();
+
+        LineOfSightChecker.OnGainSight += HandleGainSight;
+        LineOfSightChecker.OnLoseSight += HandleLoseSight;
+    }
+
+    private void HandleGainSight(Transform Target){
+        if (MovementCoroutine != null){
+            StopCoroutine(MovementCoroutine);
+        }
+
+        MovementCoroutine = StartCoroutine(Hide(Target));
+    }
+
+    private void HandleLoseSight(Transform Target){
+        if (MovementCoroutine != null){
+            StopCoroutine(MovementCoroutine);
+        }
+        // Whenever the agent loses sight
+        //      Will stop running hide coroutine and stand still
+        //          - Might want to add more interesting behavior in future
+    }
+
+    private IEnumerator Hide(Transform Target){
+        while(true){
+            // \/ All the geometry we might want to hide behind
+            int hits = Physics.OverlapSphereNonAlloc(Agent.transform.position, LineOfSightChecker.Collider.radius, Colliders, HidableLayers);
+
+            for(int i=0;i < hits; i++){
+                if (NavMesh.SamplePosition(Colliders[i].transform.position, out NavMeshHit hit, 2f, Agent.areaMask)){
+                    if (!NavMesh.FindClosestEdge(hit.position, out hit, Agent.areaMask)){
+                        Debug.LogError($"Unable to find an edge close to {hit.position}");   
+                    }
+                    
+                    if (Vector3.Dot(hit.normal, (Target.position - hit.position).normalized) < HideSensitivity){
+                        Agent.SetDestination(hit.position);
+                        break;
+                    }
+                    else {
+                        // Side we chose was facing the player, we will try again on the other side of the object
+                        // 2 here assumes the object is 2 units wide or narrower. We might need to change it for our game.
+                        if (NavMesh.SamplePosition(Colliders[i].transform.position - (Target.position - hit.position).normalized * 2, out NavMeshHit hit2, 2f, Agent.areaMask)){
+                            if (!NavMesh.FindClosestEdge(hit2.position, out hit2, Agent.areaMask)){
+                                Debug.LogError($"Unable to find an edge close to {hit2.position} (second attempt)");   
+                            }
+                    
+                            if (Vector3.Dot(hit2.normal, (Target.position - hit2.position).normalized) < HideSensitivity){
+                                Agent.SetDestination(hit2.position);
+                                break;
+                            }
+                        }
+                    }
+                }
+                else {
+                    Debug.LogError($"Unable to find a NavMesh near the object {Colliders[i].name} at {Colliders[i].transform.position}");
+                }
+            }
+            yield return null; 
+        }
+    }
+
+}
